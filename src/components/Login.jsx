ad5881fab73a4cf262abbc74c7db4a676ac3e38a
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-01 11:01:59 +0300

    fetched user data using the builtin FormData

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,39 @@
+// import { useRef } from "react";
+
 export default function Login() {
+  // const emailRef = useRef();
+  // const passwordRef= useRef();
+
+function handleSubmit(e){
+  e.preventDefault();
+  //get the value of email and password from the input fields using useRef
+
+  // const email = emailRef.current.value;
+  // const password = passwordRef.current.value;
+  // console.log({email:email,password:password});
+
+//  emailRef.current.value="";
+//  passwordRef.current.value="";
+ const formData = new FormData(e.target);
+ const data = Object.fromEntries(formData.entries());
+ console.log(data);
+
+}
+  
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email"  />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"/>
         </div>
       </div>
 
