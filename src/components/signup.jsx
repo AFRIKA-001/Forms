3eb0d99eaf71866914273e29934a8f278872e2a8
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-01 11:22:10 +0300

    collect user data using the in built FormData method

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,6 +1,20 @@
 export default function Signup() {
+
+function handleSubmit(e){
+    e.preventDefault();
+
+ const formData = new FormData(e.target);
+ const aquisitionData = formData.getAll("acquisition");
+ const data = Object.fromEntries(formData.entries());
+ data.aquisition = aquisitionData;
+ console.log(data);
+}
+
+
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
